3a5bcc6912a028983a655d7e01822fdac9371b84
Zel <jules.hermyer@gmail.com>
2022-08-29 01:19:03 +0200

    mostly finished, need to make it work for treasures

--- d_and_d.c
+++ d_and_d.c
@@ -1,27 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <SFML/Audio.h>
-#include <SFML/Graphics.h>
 
 typedef uint64_t board;
 typedef bool map[64];
 typedef char constraint[8];
-	sfVideoMode mode = {864, 864, 220};
-	sfRenderWindow* window;
-	sfTexture* texture_wall;
-	sfTexture* texture_treasure;
-	sfTexture* texture_monster;
-	sfTexture* texture_empty;
-	sfSprite* wall;
-	sfSprite* empty_sprite;
-	sfFont* font;
-	sfSprite* treasure;
-	sfSprite* monster_sprite;
-	sfVector2f scale;
-	sfEvent event;
-	sfTime wait;
+
 struct problem {
     board monsters;
     board treasures;
@@ -303,56 +288,9 @@ bool test_treasure_rooms(problem *p, board *b){
 
 
 
-
-void draw_board(problem* p, board b, sfSprite* wall, sfSprite* monster_sprite, sfSprite* treasure, sfSprite* empty_sprite, sfFont* font,sfRenderWindow* window){
-	char str[96];
-	sfVector2f pos;
-	sfVector2f postext;
-	postext.x = 0;
-	postext.y = 0;
-	sfText* text;
-	text = sfText_create();
-	sfText_setColor(text, sfWhite);
-	sfText_setFont(text,font);
-	sfText_setCharacterSize(text,96);
-	sprintf(str," %d%d%d%d%d%d%d%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d",p->rows[0], p->rows[1], p->rows[2], p->rows[3], p->rows[4], p->rows[5], p->rows[6], p->rows[7], p->columns[0], p->columns[1], p->columns[2], p->columns[3], p->columns[4], p->columns[5], p->columns[6], p->columns[7]);
-	sfText_setPosition(text,postext);
-	sfText_setString(text,str);
-	sfRenderWindow_drawText(window,text,NULL);
-	pos.x = 0;
-	pos.y = 96;
-    for(int i = 0; i < 64; i++)
-    {
-		if(i%8 == 0 && i!= 0) {
-			pos.y = pos.y + 16*6;
-			pos.x = 96;
-		}
-		else pos.x = pos.x + 16*6;
-			sfSprite_setPosition(empty_sprite,pos);
-			sfRenderWindow_drawSprite(window, empty_sprite, NULL);
-        if (((p->monsters >> i) & 1) != 0){
-			sfSprite_setPosition(monster_sprite, pos);
-			sfRenderWindow_drawSprite(window, monster_sprite, NULL);
-        }
-		else if (((p->treasures >> i) & 1) != 0){
-			sfSprite_setPosition(treasure, pos);
-			sfRenderWindow_drawSprite(window, treasure, NULL);
-        }
-		else if (((b >> i) & 1) != 0){
-			sfSprite_setPosition(wall,pos);
-            sfRenderWindow_drawSprite(window, wall, NULL);
-		}
-	}
-	sfText_destroy(text);
-}
-	
 void solve(problem *p, int row, board b)
 {
-	sfRenderWindow_clear(window, sfBlack);
-	draw_board(p, b, wall, monster_sprite, treasure, empty_sprite,font, window);
-   	sfRenderWindow_display(window);
-	sfSleep(wait);
-   	 // Unsolvable from this board
+    // Unsolvable from this board
     if (test_columns_overload(p, b))
         return;
     //printf("testcol\n");
@@ -401,9 +339,9 @@ void solve(problem *p, int row, board b)
     }
 }
 
-int main(){
-	wait = sfSeconds(.1);
-for(int i = 0; i < 256; i++){
+int main(void)
+{
+    for(int i = 0; i < 256; i++){
         enum_popcount_byte[i] = popcount(i);
     
     }
@@ -413,44 +351,9 @@ for(int i = 0; i < 256; i++){
         &p7, &p8, &p9
     };
 
-		scale.x = 6;
-	scale.y = 6;
-	//loading the textures
-		texture_wall = sfTexture_createFromFile("./ressources/wall.png", NULL);
-		texture_treasure = sfTexture_createFromFile("./ressources/treasure.png", NULL);	
-	texture_monster = sfTexture_createFromFile("./ressources/monster.png", NULL);
-	texture_empty = sfTexture_createFromFile("./ressources/empty.png", NULL);
-	//creating the sprites
-	wall = sfSprite_create();
-	treasure =sfSprite_create();
-	empty_sprite = sfSprite_create();
-	monster_sprite =sfSprite_create();
-	//setting the texture
-	sfSprite_setTexture(wall,texture_wall,sfTrue);
-	sfSprite_setTexture(treasure, texture_treasure, sfTrue);
-	sfSprite_setTexture(empty_sprite, texture_empty, sfTrue);
-	sfSprite_setTexture(monster_sprite,texture_monster, sfTrue);
-	//setting the scale
-	sfSprite_setScale(monster_sprite,scale);
-	sfSprite_setScale(empty_sprite,scale);
-	sfSprite_setScale(treasure,scale);
-	sfSprite_setScale(wall,scale);
-	//destroying everything
-	//creating the font
-	font = sfFont_createFromFile("./ressources/pixel.ttf");
-	//creating the window
-	window = sfRenderWindow_create(mode, "dungeons and diagrams", sfResize | sfClose, NULL);
-
-    //16*16 tiles scaled by 6, 9*9 grid
-	while(sfRenderWindow_isOpen(window)){
-		while(sfRenderWindow_pollEvent(window, &event)){
-			/*close window: exit*/
-			if(event.type == sfEvtClosed)
-				sfRenderWindow_close(window);
-		}
-		sfRenderWindow_clear(window, sfBlack);
-		solve(problems[7],0,0);
-	sfRenderWindow_display(window);
-	}
-	return 0;
+    for (int i = 0; i < 9; i++)
+    {
+        printf("Solving problem %d\n", i+1);
+        solve(problems[i], 0, 0);
+    }
 }
